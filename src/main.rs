e51a2244f4b38a3842b0866e4f493210fce0b631
Satoshi Moriai <satoshi.moriai@gmail.com>
2021-06-17 01:20:10 +0900

    Simplify.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{error,env};
 use std::fs::{self,File};
 use std::time::SystemTime;
-use chrono::prelude::{DateTime,Local};
 use nix::time::ClockId;
 use nix::sys::time::TimeSpec;
 #[cfg(not(target_os = "macos"))]
@@ -33,25 +32,15 @@ fn default_timestamp() -> &'static str {
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {    
-    let args: Vec<String> = env::args().collect();
-    let timestamp = if args.len() < 2 { default_timestamp() } else { &args[1] };
-
-    println!("timestamp file: {}", timestamp);
+    let timestamp = default_timestamp();
 
     let stime = SystemTime::now();
     { let _ = File::create(timestamp)?; }
     let etime = SystemTime::now();
     let estimated = stime + (etime.duration_since(stime)?/2);
 
-    #[cfg(debug)]
-    println!("    stime: {}", DateTime::<Local>::from(stime).to_rfc3339());
-    #[cfg(debug)]
-    println!("    etime: {}", DateTime::<Local>::from(etime).to_rfc3339());
-
     let metadata = fs::metadata(timestamp)?;
     if let Ok(mtime) = metadata.modified() {
-        println!("timestamp: {}", DateTime::<Local>::from(mtime).to_rfc3339());
-        println!("  current: {}", DateTime::<Local>::from(estimated).to_rfc3339());
         if let Ok(diff) = mtime.duration_since(estimated) {
             println!("difference: {:?} behind", diff);
             let newtime = SystemTime::now() + diff;
