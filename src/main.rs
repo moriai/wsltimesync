f49b68a166c5c3682cb03a959fd95698a4b85756
Satoshi Moriai <satoshi.moriai@gmail.com>
2021-07-11 14:23:09 +0900

    Simplify further.

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,36 @@ use std::fs::{self,File};
 use std::time::SystemTime;
 use nix::time::ClockId;
 use nix::sys::time::TimeSpec;
-#[cfg(not(target_os = "macos"))]
-use nix::time;
-#[cfg(target_os = "macos")]
-mod macos;
-#[cfg(target_os = "macos")]
-use macos::time;
+#[cfg(target_os = "macos")] mod macos;
+#[cfg(target_os = "macos")] use macos::time;
+#[cfg(not(target_os = "macos"))] use nix::time;
 
 trait SetTime {
-    fn set_time(&self) -> Result<(), Box<dyn error::Error>>;
+    fn set(&self) -> Result<(), Box<dyn error::Error>>;
 }
 
 impl SetTime for SystemTime {
-    fn set_time(&self) -> Result<(), Box<dyn error::Error>> {
-        let unixtime = self.duration_since(SystemTime::UNIX_EPOCH)?;
-        time::clock_settime(ClockId::CLOCK_REALTIME, TimeSpec::from(unixtime))?;
+    fn set(&self) -> Result<(), Box<dyn error::Error>> {
+        let unix_time = self.duration_since(SystemTime::UNIX_EPOCH)?;
+        time::clock_settime(ClockId::CLOCK_REALTIME, TimeSpec::from(unix_time))?;
         Ok(())
     }
 }
 
-fn default_timestamp() -> &'static str {
-    if let Ok(dir) = env::var("USERPROFILE") {
-        let fname = dir + "/.wsltimestamp";
-        Box::leak(fname.into_boxed_str())
-    } else {
-        "timestamp"
-    }
-}
-
 fn main() -> Result<(), Box<dyn error::Error>> {    
-    let timestamp = default_timestamp();
+    let timestamp = env::var("USERPROFILE")? + "/.wsltimestamp";
 
-    let stime = SystemTime::now();
-    { let _ = File::create(timestamp)?; }
-    let etime = SystemTime::now();
-    let estimated = stime + (etime.duration_since(stime)?/2);
+    let start_time = SystemTime::now();
+    { let _ = File::create(&timestamp)?; }
+    let end_time = SystemTime::now();
+    let estimated = start_time + (end_time.duration_since(start_time)?/2);
 
-    let metadata = fs::metadata(timestamp)?;
+    let metadata = fs::metadata(&timestamp)?;
     if let Ok(mtime) = metadata.modified() {
         if let Ok(diff) = mtime.duration_since(estimated) {
             println!("difference: {:?} behind", diff);
-            let newtime = SystemTime::now() + diff;
-            newtime.set_time()?;
+            let new_time = SystemTime::now() + diff;
+            new_time.set()?;
         } else {
             let diff = estimated.duration_since(mtime)?;
             println!("difference: {:?} ahead", diff);
