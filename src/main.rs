4f88091b82176527ccfc33b97e2580883a2969b7
Satoshi Moriai <satoshi.moriai@gmail.com>
2021-07-11 14:48:12 +0900

    Correct variable & method names.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,18 @@ use std::time::SystemTime;
 use chrono::prelude::{DateTime,Local};
 use nix::time::ClockId;
 use nix::sys::time::TimeSpec;
-#[cfg(not(target_os = "macos"))]
-use nix::time;
-#[cfg(target_os = "macos")]
-mod macos;
-#[cfg(target_os = "macos")]
-use macos::time;
+#[cfg(target_os = "macos")] mod macos;
+#[cfg(target_os = "macos")] use macos::time;
+#[cfg(not(target_os = "macos"))] use nix::time;
 
 trait SetTime {
-    fn set_time(&self) -> Result<(), Box<dyn error::Error>>;
+    fn set(&self) -> Result<(), Box<dyn error::Error>>;
 }
 
 impl SetTime for SystemTime {
-    fn set_time(&self) -> Result<(), Box<dyn error::Error>> {
-        let unixtime = self.duration_since(SystemTime::UNIX_EPOCH)?;
-        time::clock_settime(ClockId::CLOCK_REALTIME, TimeSpec::from(unixtime))?;
+    fn set(&self) -> Result<(), Box<dyn error::Error>> {
+        let unix_time = self.duration_since(SystemTime::UNIX_EPOCH)?;
+        time::clock_settime(ClockId::CLOCK_REALTIME, TimeSpec::from(unix_time))?;
         Ok(())
     }
 }
@@ -38,24 +35,24 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     println!("timestamp file: {}", timestamp);
 
-    let stime = SystemTime::now();
+    let start_time = SystemTime::now();
     { let _ = File::create(timestamp)?; }
-    let etime = SystemTime::now();
-    let estimated = stime + (etime.duration_since(stime)?/2);
+    let end_time = SystemTime::now();
+    let estimated = start_time + (end_time.duration_since(start_time)?/2);
 
     #[cfg(debug)]
-    println!("    stime: {}", DateTime::<Local>::from(stime).to_rfc3339());
+    println!("     start: {}", DateTime::<Local>::from(start_time).to_rfc3339());
     #[cfg(debug)]
-    println!("    etime: {}", DateTime::<Local>::from(etime).to_rfc3339());
+    println!("       end: {}", DateTime::<Local>::from(end_time).to_rfc3339());
 
     let metadata = fs::metadata(timestamp)?;
     if let Ok(mtime) = metadata.modified() {
-        println!("timestamp: {}", DateTime::<Local>::from(mtime).to_rfc3339());
-        println!("  current: {}", DateTime::<Local>::from(estimated).to_rfc3339());
+        println!(" timestamp: {}", DateTime::<Local>::from(mtime).to_rfc3339());
+        println!("   current: {}", DateTime::<Local>::from(estimated).to_rfc3339());
         if let Ok(diff) = mtime.duration_since(estimated) {
             print!("difference: {:?} behind", diff);
-            let newtime = SystemTime::now() + diff;
-            if newtime.set_time().is_ok() {
+            let new_time = SystemTime::now() + diff;
+            if new_time.set().is_ok() {
                 println!(" -- adjusted");
             } else {
                 println!();
